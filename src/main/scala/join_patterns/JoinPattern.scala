9ce34c84db6caf93cbb371de99de6dbc98a9383f
Antoine <antoine.sb@orange.fr>
2022-03-17 16:08:15 +0100

    add checkGuard()

--- src/main/scala/join_patterns/JoinPattern.scala
+++ src/main/scala/join_patterns/JoinPattern.scala
@@ -12,8 +12,22 @@ def _println[T](x: Expr[T])(using Quotes) = {
 	println(prettyPrint(tree))
 }
 
+def checkGuard(using Quotes)(guard: Option[quotes.reflect.Term]): Boolean = {
+	import quotes.reflect.*
+
+	guard match
+		case Some(Apply(Select(_, "apply"), _)) =>
+			//println("[Join-Patterns] Guard is function application")
+			true
+		case Some(_) =>
+			Console.err.println("[Join-Patterns] Error: Guard was not a function application")
+			false
+		case None =>
+			//println("[Join-Patterns] No guard")
+			true
+}
+
 /**
- * Check that casedef guard is an Apply()
  * https://docs.scala-lang.org/scala3/reference/metaprogramming/macros.html#pattern-matching-on-quoted-expressions
 */
 def _match[T](x: Expr[T])(using Quotes) = {
@@ -22,20 +36,44 @@ def _match[T](x: Expr[T])(using Quotes) = {
 	x.asTerm match
 		case Inlined(_, _, Block(_, Block(n, _))) => {
 			n(0) match
-				case DefDef(_, _, _, Some(b)) => b match
-					case Block(_, Match(_, cases)) => {
-						for (_case <- cases)
-							println(prettyPrint(_case))
+				case DefDef(_, _, _, Some(Block(_, Match(_, cases)))) => {
+					for (_case <- cases) {
+						_case match
+							case CaseDef(pattern, guard, _) =>
+								//println(prettyPrint(guard))
+								checkGuard(guard)
 					}
+				}
 		}
 
 	// generate code
 	x
 }
 
+def _match2(x: Expr[Any])(using Quotes) = {
+	import quotes.reflect.*
+
+	x match
+		case '{ $y: (Message => Int) } =>
+			println(y)
+			print("first")
+		case '{ $block0: t } =>
+			println(Type.show[t])
+			println()
+			println(block0)
+			print("second")
+
+/*
+			block0 match
+				case '{($stmts: List[t1], $trm: t2)} => println(stmts)
+				*/
+
+	x
+}
+
 // top-level
 inline def receive[R](queue: LinkedTransferQueue[Message])(inline expr : R): Unit = ${
-	_match('{expr}) // check well-formed
+	_match('{expr})
 }
 
 /*
@@ -45,7 +83,7 @@ val p = (C c -> D d)
 val p1[Ty] = Ty c
 
 receive(e) {
-	D d => println(""),
+	d: D => println(""),
 	D(d0, d1, d2) => println(""),
 	(A a, B b, C c) => println(""), // local copies, used data is marked for `this` but not consumed (so others can use it)
 	(A a & B b & C c) => println(""), // all present at the same time in queue
--- src/main/scala/join_patterns/main.scala
+++ src/main/scala/join_patterns/main.scala
@@ -26,9 +26,12 @@ def main(): Unit =
 	val queue: LinkedTransferQueue[Message] = LinkedTransferQueue[Message]()
 	val i: Int = 0;
 
+	val iZero: Int => Boolean = (i: Int) => i == 0
+	val iOne: Int => Boolean = (i: Int) => i == 0
+
 	receive (queue) {
 		(x: Message) => x match
-			case c: Any if i == 0 => 8
-			case c: Any if i == 1 => 9
+			case c: Any if iZero(i) => 8
+			case c: Any if iOne(i) => 9
 	}
 
