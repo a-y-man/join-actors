aed6aa78f4b5165d8b2f39cbe8e6d6932b2b6cb2
Antoine <antoine.sb@orange.fr>
2022-05-17 18:01:15 +0200

    add  tests

--- src/test/scala/join_patterns/UnitTests.scala
+++ src/test/scala/join_patterns/UnitTests.scala
@@ -4,17 +4,16 @@ import org.scalatest.funsuite.AnyFunSuite
 import scala.util.Random
 import java.util.concurrent.LinkedTransferQueue
 
-/*
-Does not work if placed inside the class. Maybe a bug ?
-*/
-sealed abstract class Msg
-case class A() extends Msg
-case class B(n: Int) extends Msg
-case class C(n: String) extends Msg
-
 class UnitTests extends AnyFunSuite {
+  sealed abstract class Msg
+  case class A() extends Msg
+  case class B(n: Int) extends Msg
+  case class C(n: String) extends Msg
+  case class D() extends Msg
+  case class E() extends Msg
+  case class F(b: Int, a: String) extends Msg
 
-  test("Single Empty Class, no predicate") {
+  test("Single Empty Class, no Predicate") {
     val result = Random.nextInt
     val rcv = receive { (y: Msg) => y match
       case A() => result
@@ -26,7 +25,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) === result)
   }
 
-  test("Single Empty Class, predicate") {
+  test("Single Empty Class, Predicate") {
     val result = Random.nextInt
     val ifZero = (i: Int) => i == 0
     val rcv = receive { (y: Msg) => y match
@@ -40,7 +39,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Wildcard, no predicate") {
+  test("Wildcard, no Predicate") {
     val result = Random.nextInt
     val rcv = receive { (y: Msg) => y match
       case _ => result
@@ -52,7 +51,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Wildcard, predicate") {
+  test("Wildcard, Predicate") {
     val result = Random.nextInt
     val ifZero = (i: Int) => i == 0
     val rcv = receive { (y: Msg) => y match
@@ -66,7 +65,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Single Class, One Int Member, no predicate") {
+  test("Single Class, One Int Member, no Predicate") {
     val result = Random.nextInt
     val rcv = receive { (y: Msg) => y match
       case B(n: Int) => n
@@ -78,7 +77,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Single Class, One Int Member, predicate") {
+  test("Single Class, One Int Member, Predicate") {
     val result = Random.nextInt
     val ifZero = (i: Int) => i == 0
     val rcv = receive { (y: Msg) => y match
@@ -92,7 +91,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Single Class, One String Member, no predicate") {
+  test("Single Class, One String Member, no Predicate") {
     //val result = Random.alphanumeric.filter(_.isLetter).take((Random.nextInt % 5) + 1).mkString
     val result = "test"
     val rcv = receive { (y: Msg) => y match
@@ -105,7 +104,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Single Class, One String Member, predicate") {
+  test("Single Class, One String Member, Predicate") {
     //val result = Random.alphanumeric.filter(_.isLetter).take((Random.nextInt % 5) + 1).mkString
     val result = "test"
     val ifNotEmpty = (i: String) => !i.isEmpty
@@ -120,7 +119,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result)
   }
 
-  test("Single Class, One Int and One String Members, no predicate") {
+  test("Single Class, One Int and One String Members, no Predicate") {
     val result = "test "
     val rep = Random.nextInt(5)
     val rcv = receive { (y: Msg) => y match
@@ -133,7 +132,7 @@ class UnitTests extends AnyFunSuite {
     assert(rcv(q) == result.repeat(rep))
   }
 
-  test("Single Class, One Int and One String Members, predicate") {
+  test("Single Class, One Int and One String Members, Predicate") {
     val result = "test "
     val rep = Random.nextInt(5)
     val isZero: Int => Boolean = (n: Int) => n == 0
@@ -146,5 +145,52 @@ class UnitTests extends AnyFunSuite {
     q.add(F(rep, result))
 
     assert(if rep == 0 then rcv(q) == result else rcv(q) == result.repeat(rep))
+  }
+
+  // BUG result + 3
+  test("Multiple Empty Classes, no Predicate") {
+    val result = Random.nextInt
+    val rcv = receive { (y: Msg) => y match
+      case (A(), D(), E()) => result
+      case (A(), D()) => result + 1
+      case (D(), E()) => result + 2
+      case D() => result + 3
+      case E() => result + 4
+    }
+    val q = LinkedTransferQueue[Msg]
+
+    q.add(A())
+    q.add(D())
+    q.add(E())
+
+    assert(rcv(q) == result)
+  }
+
+  test("One Tupled Empty Class, no Predicate") {
+    val result = Random.nextInt
+    val rcv = receive { (y: Msg) => y match
+      case (A()) => result
+    }
+    val q = LinkedTransferQueue[Msg]
+
+    q.add(A())
+
+    assert(rcv(q) == result)
+  }
+
+  test("Multiple Empty Classes, Predicate") {
+    val result = Random.nextInt
+    val isZero: Int => Boolean = (n: Int) => n == 0
+    val rcv = receive { (y: Msg) => y match
+      case (A(), D(), E()) if isZero(0) => result + 1
+      case (A(), D(), E()) => result
+    }
+    val q = LinkedTransferQueue[Msg]
+
+    q.add(A())
+    q.add(D())
+    q.add(E())
+
+    assert(rcv(q) == result + 1)
   }
 }
