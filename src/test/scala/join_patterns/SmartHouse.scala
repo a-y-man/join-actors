2eb77b7c9fb8d9bcd474f550b75c72f05108a700
Antoine <antoine.sb@orange.fr>
2022-05-25 19:00:05 +0200

    remove unused tests

--- src/test/scala/join_patterns/SmartHouse.scala
+++
@@ -1,9 +0,0 @@
-package join_patterns
-
-import org.scalatest.funsuite._
-import java.util.concurrent.LinkedTransferQueue
-
-class SmartHouseTest() extends AnyFunSuite {
-	test("SmartHouse") {
-	}
-}
