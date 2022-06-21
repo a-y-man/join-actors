260e2b261bf8710b7abdca9ecb7b86a3aa74998a
Antoine <antoine.sb@orange.fr>
2022-06-21 12:34:59 +0200

    remove assertion functions

--- src/main/scala/join_patterns/util.scala
+++ src/main/scala/join_patterns/util.scala
@@ -9,12 +9,6 @@ def to[T: Type, R: Type](f: Expr[T] => Expr[R])(using Quotes): Expr[T => R] = '{
 def from[T: Type, R: Type](f: Expr[T => R])(using Quotes): Expr[T] => Expr[R] = (x: Expr[T]) =>
   '{ $f($x) }
 
-inline def assert(inline expr: Boolean): Unit = ${ assertImpl('expr) }
-
-def assertImpl(expr: Expr[Boolean])(using Quotes): Expr[Unit] = '{
-  if ! $expr then println(s"failed assertion: ${${ showExpr(expr) }}")
-}
-
 def showExpr[T](expr: Expr[T])(using Quotes): Expr[String] =
   val code: String = expr.show
   Expr(code)
