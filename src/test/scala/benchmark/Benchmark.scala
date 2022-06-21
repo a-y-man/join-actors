9fd7804d6a202ec70bca5acac4886146a462f667
Antoine <antoine.sb@orange.fr>
2022-06-21 15:32:36 +0200

    add export to file + add boxplot data transformer

--- src/test/scala/benchmark/Benchmark.scala
+++ src/test/scala/benchmark/Benchmark.scala
@@ -1,6 +1,5 @@
 package test.benchmark
 
-import scala.concurrent.duration.DurationLong
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
@@ -22,15 +21,14 @@ class BenchmarkPass(
       Duration.Inf
     )
 
-  def benchmark(iterations: Int): Long =
+  def benchmark(iterations: Int): Seq[Long] =
     Await
       .result(
         Future.sequence((0 to iterations).map(_ => mainFn())),
         Duration.Inf
       )
-      .sum
 
-  def run(warmupIterations: Int, iterations: Int): Long =
+  def run(warmupIterations: Int, iterations: Int): Seq[Long] =
     println(f"-- Pass $name")
 
     println("\tstart warmup")
@@ -53,56 +51,82 @@ class Benchmark(
     private val nullPass: BenchmarkPass,
     private val passes: Seq[BenchmarkPass]
 ) {
-  def run: Long =
+  private val folder = "data"
+  private val sep    = ';'
+
+  def displayResults(results: List[(String, Seq[Long])]) =
     import Console.{GREEN, RED, RESET}
 
-    println(
-      f"Benchmark $name BEGIN (iterations: $iterations, warmup iterations: $warmupIterations)"
-    )
-
-    val results: Map[String, Long] =
-      Map((nullPass.name, nullPass.run(warmupIterations, iterations))).concat(
-        passes
-          .map(p =>
-            val res = (p.name, p.run(warmupIterations, iterations))
-            println
-            res
-          )
-      )
-
-    println(f"Benchmark $name END")
-
-    val total               = results.values.sum
+    val total               = results.map(_._2.sum).sum
     val average             = total / results.size
-    val (nullName, nullRes) = results.head
+    val (nullName, nullRes) = (results(0)._1, results(0)._2.sum)
 
     println(
       f"Benchmark $name RESULTS" +
-        "\n" + f"total elapsed time: ${total.nano.toSeconds}s" +
-        "\n" + f"average time per pass: ${average.nano.toSeconds} s" + '\n'
+        "\n" + "total elapsed time: " + "%.2f".format(total / 1e9) + "s" +
+        "\n" + "average time per pass: " + "%.2f".format(average / 1e9) + "s" + '\n'
     )
 
     val averagePerIt  = "%.2f".format(nullRes.toDouble / iterations)
-    val averagePerItS = "%.2f".format(nullRes.nano.toSeconds.toDouble / iterations)
+    val averagePerItS = "%.4f".format((nullRes.toDouble / iterations) / 1e9)
     println(
       f"Pass $nullName" +
-        "\n\t" + f"elapsed time : ${nullRes.nano.toSeconds} s" +
+        "\n\t" + f"elapsed time : " + "%.2f".format(nullRes / 1e9) + "s" +
         "\n\t" + f"average time per iteration : $averagePerIt ns / $averagePerItS s" + '\n'
     )
 
     for (passName, passResult) <- results.tail do
-      val averagePerIt  = "%.2f".format(passResult.toDouble / iterations)
-      val averagePerItS = "%.2f".format(passResult.nano.toSeconds.toDouble / iterations)
-      val delta         = ((passResult - nullRes) * 100.0) / nullRes.toDouble
+      val res           = passResult.sum
+      val averagePerIt  = "%.2f".format(res.toDouble / iterations)
+      val averagePerItS = "%.4f".format((res.toDouble / iterations) / 1e9)
+      val delta         = ((res - nullRes) * 100.0) / nullRes.toDouble
       val delta_formatted =
         (if delta < 0 then s"${GREEN}" else s"${RED}") + "%.2f".format(delta) + s"${RESET}"
 
       println(
         f"Pass $passName" +
-          "\n\t" + f"elapsed time : ${passResult.nano.toSeconds} s" +
+          "\n\t" + f"elapsed time : " + "%.2f".format(res / 1e9) + "s" +
           "\n\t" + f"average time per iteration : $averagePerIt ns / $averagePerItS s" +
           "\n\t" + f"pass speed related to null pass: $delta_formatted " + '%'
       )
 
-    total
+  def boxplot(results: List[(String, Seq[Long])]): List[(String, Seq[Long])] =
+    results.map((n, t) =>
+      val _t = t.sorted
+      val q1 = _t((_t.size * 0.25).ceil.toInt)
+      val md = _t((_t.size * 0.5).ceil.toInt)
+      val q3 = _t((_t.size * 0.75).ceil.toInt)
+
+      (n, List(_t(0), q1, md, q3, _t.last))
+    )
+
+  def toFile(results: List[(String, Seq[Long])]) =
+    import java.util.Date
+    import java.io.{File, PrintWriter}
+    import java.text.SimpleDateFormat
+
+    val timestamp = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(Date())
+    val file      = PrintWriter(File(f"$folder/${name}_$timestamp.csv"))
+
+    file.write(
+      results.map((name, times) => name + sep + times.mkString(sep.toString)).mkString("\n")
+    )
+    file.close
+
+  def run: Long =
+    println(
+      f"Benchmark $name BEGIN (iterations: $iterations, warmup iterations: $warmupIterations)"
+    )
+
+    val results: List[(String, Seq[Long])] =
+      List((nullPass.name, nullPass.run(warmupIterations, iterations))).concat(
+        passes.map(p => (p.name, p.run(warmupIterations, iterations)))
+      )
+
+    println(f"Benchmark $name END")
+
+    displayResults(results)
+    toFile(boxplot(results))
+
+    results.map(_._2.sum).sum
 }
