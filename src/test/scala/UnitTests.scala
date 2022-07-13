eaf05ed30868848e3e26c6421e74860ef2f3ecec
Antoine <antoine.sb@orange.fr>
2022-07-13 15:54:52 +0200

    add dynamic join pattern test

--- src/test/scala/UnitTests.scala
+++ src/test/scala/UnitTests.scala
@@ -317,6 +317,31 @@ class Optionalfeatures extends UnitTests {
 
     assert(rcv(q) == (if result3 then result2 else result0))
   }
+
+  test("Dynamic join patterns") {
+    val (result0, result1, result2, result3) =
+      (Random.nextInt, Random.nextInt.toString, Random.nextInt, Random.nextBoolean)
+    var rcv = receive { (y: Msg) =>
+      y match
+        case A() => result0
+        case _   => result1
+    }
+    val q = LinkedTransferQueue[Msg]
+
+    q.add(A())
+    val outcome0 = rcv(q)
+
+    rcv = receive { (y: Msg) =>
+      y match
+        case D() => result2
+        case _   => result3
+    }
+
+    q.add(D())
+    val outcome1 = rcv(q)
+
+    assert(outcome0 == result0 && outcome1 == result2)
+  }
 }
 
 class JoinPatternOrdering extends UnitTests {
