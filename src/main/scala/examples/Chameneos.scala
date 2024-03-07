699120815bf7aa97e3aa7455a6371c8b18ad381d
ayman <27450013+a-y-man@users.noreply.github.com>
2024-03-07 18:48:55 +0100

    initial attempt of Chameneos.scala and DiningPhilosophers.scala

--- src/main/scala/examples/Chameneos.scala
+++ src/main/scala/examples/Chameneos.scala
@@ -1,41 +1,31 @@
 package join_patterns.examples
 
-// import actor.*
-// import join_patterns.*
-// import org.scalacheck.*
-// import org.scalatest.run
+import actor.*
+import join_patterns.*
 
-// import java.util.concurrent.TimeUnit
-// import scala.compiletime.ops.int
-// import scala.concurrent.Await
-// import scala.concurrent.Future
-// import scala.concurrent.duration.Duration
 // import scala.util.*
 
-// import concurrent.ExecutionContext.Implicits.global
+enum ChameneoColor:
+  case Blue, Red, Yellow
 
-def ChameneosExample() =
-  println("Chameneos")
-  // type ChameneoRef  = ActorRef[ChameneoMsg]
-  // type MeetingPlace = ActorRef[MeetingPlaceMsg]
+type ChameneoRef  = ActorRef[ChameneoMsg]
+type MeetingPlace = ActorRef[MeetingPlaceMsg]
 
-  // sealed trait ChameneoMsg
-  // case class Chameneo(ref: ChameneoRef, color: Color) extends ChameneoMsg
+sealed trait ChameneoMsg
+case class MeetMsg(ref: ChameneoRef, color: ChameneoColor) extends ChameneoMsg
 
-  // sealed trait MeetingPlaceMsg
-  // case class Meet(ref: MeetingPlace, color: Color) extends MeetingPlaceMsg
+sealed trait MeetingPlaceMsg
+case class Meet(ref: MeetingPlace, color: ChameneoColor) extends MeetingPlaceMsg
 
-  // sealed trait Color
-  // case object Blue   extends Color
-  // case object Red    extends Color
-  // case object Yellow extends Color
+def ChameneosExample(algorithm: MatchingAlgorithm, numberOfMeetings: Int, numberOfChameneos: Int) =
 
-  // val ALGORITHM = MatchingAlgorithm.BasicAlgorithm
-
-  // val mallActor =
-  //   Actor[MeetingPlaceMsg, Int](receive { (x: MeetingPlaceMsg, mallRef: MeetingPlace) =>
-  //     x match
-  //       case (Chameneo(ch1, c1), Chameneo(ch2, c2)) if ch1 != ch2 && c1 == c2 =>
-  //         ch1 ! Chameneo(ch2, c2)
-  //         Stop(1)
-  //   }(ALGORITHM))
+  val mallActor =
+    Actor[MeetingPlaceMsg, Int] {
+      receive { (x: MeetingPlaceMsg, mallRef: MeetingPlace) =>
+        x match
+          case (MeetMsg(ch1, c1), MeetMsg(ch2, c2)) if ch1 != ch2 && c1 == c2 =>
+            ch1 ! MeetMsg(ch2, c2)
+            Stop(1)
+      }(algorithm)
+    }
+// TBC
---
+++ src/main/scala/examples/DiningPhilosophers.scala
@@ -0,0 +1,4 @@
+package join_patterns.examples
+
+object DiningPhilosophers:
+  println("TODO!")
